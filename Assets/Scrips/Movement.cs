91a62ce53762dd19583fda96ceafe17953a058d3
Steven <Steven-ptkhuu@hotmail.com>
2019-02-20 12:37:46 +0100

    project finish

---
+++ Assets/Scrips/Movement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+
+public class Movement : MonoBehaviour
+{
+    float posX;
+    float posY;
+    float posZ;
+    float moveby = 30;
+    bool moved = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        posX = transform.position.x;
+        posY = transform.position.y;
+        posZ = transform.position.z;
+        StartCoroutine(basicMovement());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    IEnumerator basicMovement()
+    {
+        while (true)
+        {
+
+            yield return new WaitForSeconds(3f);
+
+            transform.DOMove(new Vector3(posX + 30, posY, posZ), 2);
+
+            yield return new WaitForSeconds(3f);
+
+            transform.DOMove(new Vector3(posX, posY, posZ), 2);
+ 
+        }
+
+    }
+}
---
+++ Assets/Scrips/Timer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float timeLeft;
+    public Text timeText;
+    void Start()
+    {
+        timeLeft = 60;
+        Invoke("UpdateTimeText", 2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TimerDisplay();
+    }
+    void TimerDisplay()
+    {
+        timeLeft -= Time.deltaTime;
+        UpdateTimeText();
+        print(timeLeft);
+        if (timeLeft < 0)
+        {
+            Scene scene = SceneManager.GetActiveScene(); SceneManager.LoadScene(scene.name); ;
+        }
+    }
+    public void UpdateTimeText()
+    {
+        timeText.text = "Time: " + timeLeft.ToString();
+    }
+
+}
